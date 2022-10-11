617d0ec65a458c89ad034ce77212be505c710253
maksud190 <maknep3rdoct@gmail.com>
2022-10-11 19:20:28 +0600

    nav bar created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import './App.css';
-import { createBrowserRouter } from 'react-router-dom';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Statistics from './components/Statistics/Statistics';
+import Blogs from './components/Blogs/Blogs';
 
 function App() {
 
@@ -7,14 +11,28 @@ function App() {
 
     {
       path: '/',
-
+      element: <Main></Main>,
+      children:[
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blogs',
+          element: <Blogs></Blogs>
+        }
+      ]
     }
 
   ])
 
   return (
     <div className="App">
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <div>
-            <h2>Header</h2>
-        </div>
+        <nav>
+            <Link to='/'>Home</Link>
+            <Link to='/'>Topics</Link>
+            <Link to='/statistics'>Statistics</Link>
+            <Link to='/blogs'>Blogs</Link>
+        </nav>
     );
 };
 
